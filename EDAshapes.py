bbe3860b072388c2aff546909c1478732e05f8db
Daniel Ah Tou <dlahtou@gmail.com>
2018-09-14 10:26:20 -0700

    using imagedatagenerator

--- EDAshapes.py
+++ EDAshapes.py
@@ -21,6 +21,7 @@ from osgeo.gdalconst import GA_ReadOnly
 from shapely.wkt import loads
 from keras.losses import binary_crossentropy
 from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
 
 with open('data/grid_sizes.csv', 'r') as open_file:
     grids = pd.read_csv(open_file)
@@ -339,10 +340,18 @@ def train_keras_model(x, y):
 
         checkpoint_model = ModelCheckpoint('trees_model.h5', verbose=1, save_best_only=True)
 
+        
+
         adam = Adam(lr=0.0000001)
 
         model.compile(optimizer=adam, loss='binary_crossentropy', metrics=[mean_iou])
-        model.fit(x=x, y=y, epochs=8, callbacks=[stale, checkpoint_model], batch_size=4, validation_split=0.1)
+
+        datagen = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+        datagen.fit(x)
+
+        model.fit_generator(datagen.flow(x, y), batch_size=4, epochs=8)
+
+        #model.fit(x=x, y=y, epochs=8, callbacks=[stale, checkpoint_model], batch_size=4, validation_split=0.1)
 
         model.save('trees_model.h5')
 
@@ -574,8 +583,6 @@ if __name__ == '__main__':
 
     x = np.asarray(x)
     y = np.asarray(y)
-
-    print(x.shape)
 
     np.save('trees_images.npy', x[:20])
     np.save('trees_masks.npy', y[:20])
